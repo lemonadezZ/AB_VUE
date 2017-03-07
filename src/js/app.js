5f915f3ed68a368bcf5604f2eafc7fa36a900830
dev-jingdor <945627077@qq.com>
2017-03-07 01:26:20 -0500

    test vue with vuex

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,42 @@
 import Vue from 'vue/dist/vue.common.js';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+
+
+const Counter = {
+  template: `<div>{{ count }}</div>`,
+  computed: {
+    count () {
+      return  this.$store.state.count
+    }
+  }
+}
+
 
 var app = new Vue({
-  el: '#root',
-  data: {
-    message: 'Hello Vue!'
-  }
+	el: '#root',
+	store,
+	components: { Counter },
+	methods:{
+		plus:function(e){
+			store.commit('increment')	
+		},
+		reduce:function(e){
+			store.commit('increment')
+		}
+	},
+	template:'<div> <counter></counter> <a v-on:click="plus">+</a><a v-on:click="reduce">-</a> </div>',
 })
